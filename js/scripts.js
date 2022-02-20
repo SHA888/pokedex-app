c4012b5957a8bbcfc2b3e45488aa4f07d887ac9a
Kresna Sucandra <kresnasucandra@gmail.com>
2022-02-21 06:48:01 +0800

    [Feat] Added pokemonList array (JS)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert("Guees what!");
-
-let favoriteFood = "Banana";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["grass", "poison"],
+    heights: 70,
+    weight: 6.9,
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    types: "fire",
+    height: 60,
+    weight: 8.5,
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    types: "water",
+    height: 50,
+    weight: 9,
+  },
+];
