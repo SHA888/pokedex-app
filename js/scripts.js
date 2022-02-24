3f7e0b92d6f206fe709a2253d29e073cb4de7ba6
Kresna Sucandra <kresnasucandra@gmail.com>
2022-02-24 23:43:52 +0800

    [Feat] Added IIFE into pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,46 @@
-let pokemonList = [
-  {
-    id: 1,
-    name: "Bulbasaur",
-    types: ["grass", "poison"],
-    height: 70,
-    weight: 6.9,
-  },
-  {
-    id: 4,
-    name: "Charmander",
-    types: "fire",
-    height: 60,
-    weight: 8.5,
-  },
-  {
-    id: 7,
-    name: "Squirtle",
-    types: "water",
-    height: 50,
-    weight: 9,
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      id: 1,
+      name: "Bulbasaur",
+      types: ["grass", "poison"],
+      height: 70,
+      weight: 6.9,
+    },
+    {
+      id: 4,
+      name: "Charmander",
+      types: "fire",
+      height: 60,
+      weight: 8.5,
+    },
+    {
+      id: 7,
+      name: "Squirtle",
+      types: "water",
+      height: 50,
+      weight: 9,
+    },
+  ];
 
-pokemonList.forEach(function (pokemon) {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   let wow = "";
   if (pokemon.height > 60) {
     wow = " - Wow, that's huge!";
   }
-  document.write(`${pokemon.name} (height: ${pokemon.height})${wow}<br>`);
+  document.write(`<p>${pokemon.name} (height: ${pokemon.height})${wow}<p>`);
 });
